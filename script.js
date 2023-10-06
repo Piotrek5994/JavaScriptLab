775cce59c275d4f23c9e6c9a5b7ad0699dd86d1b
Piotrek5994 <piotrek5994@gmail.com>
2023-10-06 13:15:59 +0200

    First Part

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
-const liczba1 = document.querySelector('#liczba1')
-const btnPrzelicz = document.querySelector('#przelicz')
-const wynikPojemnika = document.querySelector('#wynik')
+const a = document.querySelector('#liczba1');
+const b = document.querySelector('#liczba2');
+const c = document.querySelector('#liczba3');
+const d = document.querySelector('#liczba4');
+const btnPrzelicz = document.querySelector('#przelicz');
+const wynikPojemnika = document.querySelector('#wynik');
 
-console.dir(liczba1)
-
+const sum = 0;
+const avg = 0;
+const min = 0;
+const max = 0;
 btnPrzelicz.addEventListener('click',()=>{
-    //Math.min(), Math.max()
-    const max100 = Math.min(liczba1.value, 100)
-    wynikPojemnika.innerHTML = +liczba1.value
-    //Number(liczba.value),
-    //parseInt(liczba1.value),parseFloat(liczba1.value)
-    wynikPojemnika.innerHTML = liczba1.value
-    console.log(liczba1.value)
-})
+    const sum = 
+    parseInt(a.value) + 
+    parseInt(b.value) + 
+    parseInt(c.value) + 
+    parseInt(d.value);
+
+    console.log(sum);
+    
+    const avg = ((
+        parseInt(a.value) + 
+        parseInt(b.value) + 
+        parseInt(c.value) + 
+        parseInt(d.value) )/4);
+
+        console.log(avg);
+        
+        
+        
+        //Math.min(), Math.max()
+        const min = Math.min(a.value,b.value,c.value,d.value);
+
+        console.log(min);
+        
+        const max = Math.max(a.value,b.value,c.value,d.value);
+
+        console.log(max);
+        
+        wynikPojemnika.innerHTML =
+        `Suma: ${sum} ,
+        Średnia: ${avg},
+        Minimum: ${min}, 
+        Maximum: ${max}`; 
+    })
